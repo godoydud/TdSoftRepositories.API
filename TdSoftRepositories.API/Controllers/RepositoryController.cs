5daedd172947a62824ef9b11006956ebd7edb4aa
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-13 18:06:11 -0400

    feat: Implementando RepositoryController

---
+++ TdSoftRepositories.API/Controllers/RepositoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TdSoftRepositories.API.Controllers
+{
+    [Route("repos/find")]
+    [ApiController]
+    public class RepositoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get(string nome, int pagina, int por_pagina)
+        {
+            return Ok("Hello World");
+        }
+    }
+}
