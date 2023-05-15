c8c5398e77a25335a133bfcfd91421ee5c5c2be7
Eduardo Godoy <eduardo.godoy@ufms.br>
2023-05-14 21:01:36 -0300

    Adicionando injeção de dependencias para Services

---
+++ TdSoftRepositories.API.CrossCuting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TdSoftRepositories.API.Service.Services;
+
+namespace TdSoftRepositories.API.CrossCutitng.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddTransient<RepositoryService>();
+        }
+    }
+}
