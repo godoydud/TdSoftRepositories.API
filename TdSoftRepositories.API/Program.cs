025e8c7936664654caa1e42d06622f90b3e20a8a
Eduardo Godoy <eduardo.godoy@ufms.br>
2023-05-14 21:01:18 -0300

    Limpando Program.Cs

--- TdSoftRepositories.API/Program.cs
+++ TdSoftRepositories.API/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using TdSoftRepositories.API.CrossCutitng.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigureService.Configure(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,9 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // Definir endpoint swagger para arquivo em swagger/swagger-doc.yaml para que a documenta��o de swagger possa ser acessada em https://localhost:<port>/swagger/swagger-doc.yaml (https://localhost:<port>/swagger/swagger-doc.yaml)
     app.UseSwagger();
-    // Definir endpoint swagger para arquivo em swagger/swagger-doc.yaml para que a documenta��o de swagger possa ser acessada em https://localhost:<port>/swagger/swagger-doc.yaml (https://localhost:<port>/swagger/swagger-doc.yaml)
     app.UseSwaggerUI();
 }
 
