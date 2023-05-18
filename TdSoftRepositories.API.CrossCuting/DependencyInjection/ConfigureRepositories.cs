5bf2f6945c6ff2eb1fd207ed5f5c4789d4a230b6
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-17 22:15:53 -0400

    feat: inserindo classe de configuração de repositorios

---
+++ TdSoftRepositories.API.CrossCuting/DependencyInjection/ConfigureRepositories.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TdSoftRepositories.API.Data.Context;
+using TdSoftRepositories.API.Data.Repositories;
+
+namespace TdSoftRepositories.API.CrossCutitng.DependencyInjection
+{
+    public class ConfigureRepositories
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CoreContext>(
+                options => options.UseNpgsql(configuration.GetConnectionString("Default"))
+                );
+
+            services.AddScoped(typeof(RepositoryRepository));
+        }
+    }
+}
