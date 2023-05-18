cf5415234336dfcef63dd48f485cebbaac08c947
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-17 22:15:10 -0400

    feat: implementando DbContext

---
+++ dSoftRepositories.API.Data/Context/CoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TdSoftRepositories.API.Domain.Entities;
+
+namespace TdSoftRepositories.API.Data.Context
+{
+    public class CoreContext : DbContext
+    {
+        public DbSet<Repository>? Repository { get; set; }
+
+        public CoreContext(DbContextOptions<CoreContext> options) : base(options) { }
+
+    }
+}
