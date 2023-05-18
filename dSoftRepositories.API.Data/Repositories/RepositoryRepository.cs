48dd966d68b1788d91a72171f556a14425287540
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-05-17 22:15:29 -0400

    feat: implementando repositorio para Repository

---
+++ dSoftRepositories.API.Data/Repositories/RepositoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using TdSoftRepositories.API.Data.Context;
+using TdSoftRepositories.API.Domain.Entities;
+
+namespace TdSoftRepositories.API.Data.Repositories
+{
+    public class RepositoryRepository
+    {
+        public CoreContext CoreContext { get; }
+
+        public RepositoryRepository(CoreContext context)
+        {
+            CoreContext = context;
+        }
+
+        public IQueryable GetAll() =>
+            CoreContext.Set<Repository>().AsQueryable();
+
+        public Repository GetById(Guid id) =>
+            CoreContext.Set<Repository>().Find(id);
+    }
+}
